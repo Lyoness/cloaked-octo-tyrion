e8d07ec085dcb54f79b65d1d8f2f473168ddea6e
Dan Buch <daniel.buch@gmail.com>
2015-12-18 10:27:08 -0500

    Use the collation that already exists

--- locale-test.sql
+++ locale-test.sql
@@ -2,12 +2,4 @@ SELECT words FROM locale_test ORDER BY words DESC;
 
 SELECT (words COLLATE "en_US") FROM locale_test ORDER BY words DESC;
 
-BEGIN;
-  SELECT (words COLLATE "en_GB") FROM locale_test ORDER BY words DESC;
-COMMIT;
-
-BEGIN;
-  CREATE COLLATION "en_GB" (LOCALE = 'en_GB.UTF-8');
-  SELECT (words COLLATE "en_GB") FROM locale_test ORDER BY words DESC;
-  DROP COLLATION "en_GB";
-COMMIT;
+SELECT (words COLLATE "en_GB") FROM locale_test ORDER BY words DESC;
