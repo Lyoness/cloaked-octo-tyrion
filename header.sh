cdf7bed5252d7034f450362e096d739881dd1712
Dan Buch <daniel.buch@gmail.com>
2016-11-30 14:40:59 -0500

    Try out latest mysql funcs

--- header.sh
+++ header.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 travis_configure_php_mysql56() {
-  if ! grep -qE '^socket = /run/mysql-5.6' ~/.my.cnf; then
+  if ! grep -qE '^socket = /run/mysql-5.6' $HOME/.my.cnf; then
     return
   fi
 
-  MYSQL_UNIX_PORT="$(grep ^socket ~/.my.cnf | cut -d= -f2 | tr -d ' ')"
-  export MYSQL_UNIX_PORT
+  local mysql_unix_port
+  mysql_unix_port="$(grep ^socket $HOME/.my.cnf)"
+  mysql_unix_port="${mysql_unix_port##*=}"
+  mysql_unix_port="${mysql_unix_port// /}"
 
-  if [[ -d ~/.phpenv/versions ]]; then
-    find ~/.phpenv/versions -name php.ini \
+  export MYSQL_UNIX_PORT="${mysql_unix_port}"
+
+  if [[ -d $HOME/.phpenv/versions ]]; then
+    find $HOME/.phpenv/versions -name php.ini \
       | while read -r ini_file; do
         sed -i "/^[a-z_]*mysql.*socket/s:\$: ${MYSQL_UNIX_PORT}:" "${ini_file}"
       done
   fi
 
-  if [[ -f '/etc/hhvm/php.ini' ]]; then
+  if [[ -f '//etc/hhvm/php.ini' ]]; then
     echo "
 pdo_mysql.default_socket = ${MYSQL_UNIX_PORT}
 mysqli.default_socket = ${MYSQL_UNIX_PORT}
 hhvm.mysql.socket = ${MYSQL_UNIX_PORT}
-" | tee -a '/etc/hhvm/php.ini' >/dev/null
+" | sudo tee -a '//etc/hhvm/php.ini' >/dev/null
   fi
 }
