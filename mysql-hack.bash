8751d50459550ef6360c8aa34702c44c80a3cc8e
Dan Buch <daniel.buch@gmail.com>
2016-11-30 13:32:55 -0500

    Try out php mysql header func

--- mysql-hack.bash
+++ mysql-hack.bash
@@ -4,17 +4,5 @@ set -o errexit
 set -o xtrace
 set -o pipefail
 
-MYSQL_UNIX_PORT="$(grep ^socket ~/.my.cnf | cut -d= -f2 | tr -d ' ')"
-export MYSQL_UNIX_PORT
-
-find ~/.phpenv/versions -name php.ini \
-  | while read -r ini_file; do
-    sed -i "/^[a-z_]*mysql.*socket/s:\$: ${MYSQL_UNIX_PORT}:" "${ini_file}"
-  done
-
-ini_file_addition="
-pdo_mysql.default_socket = ${MYSQL_UNIX_PORT}
-mysqli.default_socket = ${MYSQL_UNIX_PORT}
-hhvm.mysql.socket = ${MYSQL_UNIX_PORT}
-"
-echo "${ini_file_addition}" | tee -a '/etc/hhvm/php.ini' >/dev/null
+source header.sh
+travis_configure_php_mysql56
