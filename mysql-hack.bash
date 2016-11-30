72335231b1ef0d5208c2f97ddfddedab1cdc320a
Dan Buch <daniel.buch@gmail.com>
2016-11-30 13:22:30 -0500

    Hack hhvm ini better

--- mysql-hack.bash
+++ mysql-hack.bash
@@ -12,10 +12,9 @@ find ~/.phpenv/versions -name php.ini \
     sed -i "/^[a-z_]*mysql.*socket/s:\$: ${MYSQL_UNIX_PORT}:" "${ini_file}"
   done
 
-ini_file_addition='
-pdo_mysql.default_socket = __MYSQL56SOCKET__
-mysqli.default_socket = __MYSQL56SOCKET__
-hhvm.mysql.socket = __MYSQL56SOCKET__
-'
+ini_file_addition="
+pdo_mysql.default_socket = ${MYSQL_UNIX_PORT}
+mysqli.default_socket = ${MYSQL_UNIX_PORT}
+hhvm.mysql.socket = ${MYSQL_UNIX_PORT}
+"
 echo "${ini_file_addition}" | tee -a '/etc/hhvm/php.ini' >/dev/null
-sed -i "s,__MYSQL56SOCKET__,$(grep ^socket ~/.my.cnf | cut -d= -f2 | tr -d ' '),g" '/etc/hhvm/php.ini' >/dev/null
