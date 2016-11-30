ab2ed1731e7f0640489b07acdb404aa26133111f
Dan Buch <daniel.buch@gmail.com>
2016-11-30 13:14:38 -0500

    More hack

---
+++ mysql-hack.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o xtrace
+set -o pipefail
+
+MYSQL_UNIX_PORT="$(grep ^socket ~/.my.cnf | cut -d= -f2 | tr -d ' ')"
+export MYSQL_UNIX_PORT
+
+find ~/.phpenv/versions -name php.ini \
+  | while read -r ini_file; do
+    sed -i "/^[a-z_]*mysql.*socket/s:\$: ${MYSQL_UNIX_PORT}:" "${ini_file}"
+  done
+
+ini_file_addition='
+pdo_mysql.default_socket = __MYSQL56SOCKET__
+mysqli.default_socket = __MYSQL56SOCKET__
+hhvm.mysql.socket = __MYSQL56SOCKET__
+'
+echo "${ini_file_addition}" | tee -a '/etc/hhvm/php.ini' >/dev/null
+sed -i "s/__MYSQL56SOCKET__/$(grep ^socket ~/.my.cnf | cut -d= -f2 | tr -d ' ')/g" '/etc/hhvm/php.ini' >/dev/null
