f1bd3dcdd9aa7f8dc9869bf8ffe55837726c39e9
Dan Buch <daniel.buch@gmail.com>
2017-01-10 12:16:20 -0500

    Download with more vigor

--- download.py
+++ download.py
@@ -2,10 +2,23 @@ import ssl
 import sys
 import urllib.request
 
-with open(sys.argv[2], 'wb') as out:
-    out.write(
-        urllib.request.urlopen(
-            sys.argv[1],
-            context=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-        ).read()
-    )
+
+def main(sysargs=sys.argv[:]):
+    print(sys.version)
+
+    url = sysargs[1]
+    dest = sysargs[2]
+    print('---> {} => {}'.format(url, dest))
+
+    with open(sys.argv[2], 'wb') as out:
+        result = out.write(
+            urllib.request.urlopen(
+                sys.argv[1],
+                context=ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+            ).read()
+        )
+        print('     wrote bytes={}'.format(result))
+
+
+if __name__ == '__main__':
+    sys.exit(main())
