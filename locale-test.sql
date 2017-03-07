a7751909f0192b24ea6665bb3a15428a249456a4
Dan Buch <daniel.buch@gmail.com>
2017-03-07 14:55:06 -0500

    Do some bundlin

--- locale-test.sql
+++
@@ -1,5 +0,0 @@
-SELECT words FROM locale_test ORDER BY words DESC;
-
-SELECT (words COLLATE "en_US") FROM locale_test ORDER BY words DESC;
-
-SELECT (words COLLATE "en_GB") FROM locale_test ORDER BY words DESC;
