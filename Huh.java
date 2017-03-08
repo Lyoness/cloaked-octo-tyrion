086fbbcee8bcf30a0f8835e685003f7fa72356f3
Dan Buch <daniel.buch@gmail.com>
2017-03-07 20:59:25 -0500

    Try some java 8 oracle stuff

---
+++ Huh.java
@@ -0,0 +1,9 @@
+public class Huh {
+  public static void main(String[] args) {
+    if (args.length > 0) {
+      System.out.printf("oh hay %s\n", args[0]);
+    } else {
+      System.out.println("nah");
+    }
+  }
+}
